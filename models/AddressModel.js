dc6b4d7d4f55e250c9b942935bfbdcd3e14deed4
Copter <copterpanyphon@gmail.com>
2025-05-27 14:22:47 +0700

    add MongoDb

---
+++ models/AddressModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+const { Schema } = mongoose;
+
+const addressSchema = new Schema({
+    lable: {
+        type: String,
+        required: [true, "Address must have label for itself"],
+        trim: true,
+    },
+    street: {
+        type: String,
+        trim: true
+    }
+})
