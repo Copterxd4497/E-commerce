82b0e3a9e954977cbf0b2a8494542e9a7e28eaa0
Copter <copterpanyphon@gmail.com>
2025-05-29 16:44:04 +0700

    nothing

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -13,6 +13,7 @@ exports.categories = (req, res) => {
     );
     const categoriesData = fs.readFileSync(categoriesPath, "utf-8");
     const defaultCategories = JSON.parse(categoriesData);
+       
 
     res.status(200).render("categories", { defaultCategories });
   } catch (err) {
