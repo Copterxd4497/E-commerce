985c8a4d2f59db68ac15dd03eb71c4f85e580d18
Copter <copterpanyphon@gmail.com>
2025-05-29 16:38:21 +0700

    add changed file

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
 exports.productsPage = (req, res) => {
   res.status(200).render("products");
 };
 
-exports.catagories = (req, res) => {
-  res.status(200).render("categories");
+exports.categories = (req, res) => {
+  try {
+    const categoriesPath = path.join(
+      __dirname,
+      "../dev-data/defaults/defaultCategories.json"
+    );
+    const categoriesData = fs.readFileSync(categoriesPath, "utf-8");
+    const defaultCategories = JSON.parse(categoriesData);
+
+    res.status(200).render("categories", { defaultCategories });
+  } catch (err) {
+    res.status(404).send(`There is error! ${err.message}`);
+  }
 };
