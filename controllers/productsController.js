cd0f887f6ba27c82a46b45853591c2c65c73ca9f
Copter <copterpanyphon@gmail.com>
2025-05-29 22:58:28 +0700

    change many things

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,21 +1,30 @@
 const fs = require("fs");
 const path = require("path");
+const Category = require("./../models/categoriesModel");
 
 exports.productsPage = (req, res) => {
   res.status(200).render("products");
 };
 
-exports.categories = (req, res) => {
+exports.categories = async (req, res) => {
   try {
+    const categories = await Category.find();
+
     const categoriesPath = path.join(
       __dirname,
       "../dev-data/defaults/defaultCategories.json"
     );
     const categoriesData = fs.readFileSync(categoriesPath, "utf-8");
     const defaultCategories = JSON.parse(categoriesData);
-       
+    res.status(200).render("categories", { categories, defaultCategories });
 
-    res.status(200).render("categories", { defaultCategories });
+    // res.status(200).json({
+    //   message: "this is cool right",
+    //   data: {
+    //     defaultCategories,
+    //     categories,
+    //   },
+    // });
   } catch (err) {
     res.status(404).send(`There is error! ${err.message}`);
   }
